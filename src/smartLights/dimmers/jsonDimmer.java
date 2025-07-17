97ff6b92634445a821e7dcf67329c140f8b214f2
cabobalot <casey8wolfe@gmail.com>
2025-07-17 11:19:07 -0600

    json

---
+++ src/smartLights/dimmers/jsonDimmer.java
@@ -0,0 +1,63 @@
+package smartLights.dimmers;
+
+import java.io.File;
+
+import org.json.;
+
+import smartLights.RoomState;
+
+public class jsonDimmer implements Dimmer {
+
+	public jsonDimmer(File file) {
+	}
+
+	@Override
+	public RoomState dimUp() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'dimUp'");
+	}
+
+	@Override
+	public RoomState dimDown() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'dimDown'");
+	}
+
+	@Override
+	public RoomState dimMax() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'dimMax'");
+	}
+
+	@Override
+	public RoomState dimMin() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'dimMin'");
+	}
+
+	@Override
+	public RoomState firstOn() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'firstOn'");
+	}
+
+	@Override
+	public RoomState getRoomState() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getRoomState'");
+	}
+
+	@Override
+	public void setIndex(int index) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'setIndex'");
+	}
+
+	@Override
+	public int getCurrentIndex() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getCurrentIndex'");
+	}
+
+
+}
